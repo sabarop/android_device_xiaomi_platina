7eb801a92bf4542c68abf60b84ee5f406b058e20
Tri Vo <trong@google.com>
2019-02-01 11:27:25 -0800

    platina: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,8 +147,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
