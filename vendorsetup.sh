12ca39b40e52b37845850a40f27a358f2874e013
Darth9 <reyprimerjefe@gmail.com>
2020-07-15 15:37:29 +0100

    Add support for block OTA in custom ROMs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,6 +48,10 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         # use system (ROM) fingerprint where available
         export OF_USE_SYSTEM_FINGERPRINT=1
 
+	# OTA for custom ROMs
+        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
         # -- add settings for R11 --
         export FOX_R11=1
         export FOX_ADVANCED_SECURITY=1
