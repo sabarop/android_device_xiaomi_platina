23c2c81dd4405e24043ed5362513795b2e44f634
sohamxda7 <sensoham135@gmail.com>
2021-01-24 08:15:13 +0000

    clover: drop sched_enable_hmp/power_aware boot argument
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
