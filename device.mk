b6ce190dd9b2cc778010b8e40d81dbf81ec07b7f
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    platina: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: Ieb69a8c8098b7ac236338b3619d2c6cf963b82fc

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework
