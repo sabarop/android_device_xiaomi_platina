a2b7ce7a378468e6b411d72821a9fbf7e3553fb9
xyyx <xyyx@mail.ru>
2021-10-16 17:40:16 +0000

    platina: Add basic USB HAL that reports no status change
    Signed-off-by: ItsVixano <vixanonew@gmail.com>
    Change-Id: I46874b1d6a6491d07b7d5ae896a192312b4047cd

--- device.mk
+++ device.mk
@@ -369,7 +369,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.sdm660
 
 # Tethering
 PRODUCT_PACKAGES += \
