a6a3d7e35f2fa53020f6e21a7945d694f80b2ee9
Darth9 <reyprimerjefe@gmail.com>
2022-03-19 13:49:09 +0000

    Update the copyright information

--- twrp_lavender.mk
+++ twrp_lavender.mk
@@ -3,6 +3,8 @@
 # Copyright (C) 2020 The TWRP Open Source Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
+# Copyright (C) 2019-2022 The OrangeFox Recovery Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
