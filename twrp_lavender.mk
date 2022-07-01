3993ca2cbef30c8d43f19698eb24df514146ea55
Darth9 <reyprimerjefe@gmail.com>
2022-07-01 17:55:57 +0100

    Add the product release name

--- twrp_lavender.mk
+++ twrp_lavender.mk
@@ -18,6 +18,8 @@
 # limitations under the License.
 #
 
+PRODUCT_RELEASE_NAME := lavender
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
