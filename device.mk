0afcd87a3b63df724a6b94135a940185b4bfc4f7
tarsin <yuanqingxiang233@163.com>
2022-08-20 01:39:36 +0530

    platina: shims: Make shim for libMiCameraHal.so
    Logs:
    
    vndksupport: Could not load /vendor/lib/hw/camera.sdm660.so from default namespace: dlopen failed: cannot locate symbol "_ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataE" referenced by "/vendor/lib/libMiCameraHal.so"....
    
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7
    Signed-off-by: Kujou Yuko <i@779.moe>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- device.mk
+++ device.mk
@@ -143,6 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
+    libMiCameraHal_shim \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,9 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libwfdaudioclient.so" "${2}"
             "${PATCHELF}" --add-needed "libwfdmediautils.so" "${2}"
             ;;
+        vendor/lib/libMiCameraHal.so)
+            "${PATCHELF}" --add-needed "libMiCameraHal_shim.so" "${2}"
+            ;;
     esac
 }
 
