8e61c5ae552b68d93fe5877e32822de66eef6bc3
PIPIPIG233666 <pig.priv@gmail.com>
2023-02-20 15:43:50 +0800

    platina: device: Place a copy of audio platform info intcodec as audio platform info
    Silence following:
    * D platform_info: platform_info_init: Failed to open
    audio_platform_info.xml, using defaults.
    
    Change-Id: Icf9bb063d1bef72143c01009da17727f5b6fdefd

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
