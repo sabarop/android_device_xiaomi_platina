4161c6e3f6f8e0bd043693dde403a7e1bcf2f025
Sabar <31942277+sabarop@users.noreply.github.com>
2023-05-19 11:00:18 +0700

    platina: add flash auto focus permission

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
