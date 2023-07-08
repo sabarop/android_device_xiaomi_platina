bc08b75840a74e8efbbe6d33354b296188cd85de
Sabar <31942277+sabarop@users.noreply.github.com>
2023-07-08 23:21:38 +0700

    platina: Enable updating of APEXes

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ DEVICE_PATH := device/xiaomi/platina
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
