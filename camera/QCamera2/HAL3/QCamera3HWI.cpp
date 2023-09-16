ea8c96ecdce9fb7a3eee201f6f07ea112b82d1fa
Sabar <31942277+sabarop@users.noreply.github.com>
2023-09-17 01:02:47 +0700

    camera : remove IS_TYPE_VENDOR_EIS

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -3236,9 +3236,11 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
                             channel->setUBWCEnabled(true);
                         }
                         newStream->max_buffers = channel->getNumBuffers();
+#if 0
                         if (isTypeVideo == IS_TYPE_VENDOR_EIS) {
                             newStream->max_buffers = MAX_VIDEO_VENDOR_EIS_BUFFERS;
                         }
+#endif
                         newStream->priv = channel;
                     }
                     break;
@@ -8404,6 +8406,7 @@ no_error:
                     return BAD_VALUE;
                 }
             }
+#if 0
             if ((isTypePreview == IS_TYPE_VENDOR_EIS) &&
                (((1U << CAM_STREAM_TYPE_PREVIEW) == channel->getStreamTypeMask()))) {
                 //Trigger Perf Flush event to back-end
@@ -8414,6 +8417,7 @@ no_error:
                     return BAD_VALUE;
                 }
             }
+#endif
         }
     }
 
