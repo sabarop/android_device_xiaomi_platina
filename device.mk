6d7565080cec19fe9fbef1dd90bcc6cb6eda9583
Sabar <31942277+sabarop@users.noreply.github.com>
2023-11-21 07:53:05 +0700

    platina: set permission of flash-autofocus

--- device.mk
+++ device.mk
@@ -61,6 +61,10 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
