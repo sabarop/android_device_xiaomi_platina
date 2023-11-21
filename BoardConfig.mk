75ee2223b522645c4d29ee28bd2f4987fc6a31e9
Sabar <31942277+sabarop@users.noreply.github.com>
2023-11-21 09:25:52 +0700

    platina: commonize fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,9 +37,6 @@ BOARD_USES_METADATA_PARTITION := true
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
-
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
