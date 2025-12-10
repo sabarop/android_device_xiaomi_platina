a91d7729ba9afa98cf025b3ffbdafe54095e92e0
Renelz <131983767+Renelzx@users.noreply.github.com>
2025-12-10 10:58:46 +0700

    Update

--- lineage_platina.mk
+++ lineage_platina.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_WIDTH := 1080
 
@@ -15,7 +15,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_platina
+PRODUCT_NAME := lineage_platina
 PRODUCT_MODEL := MI 8 Lite
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
