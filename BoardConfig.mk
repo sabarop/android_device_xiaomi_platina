624bd0ebda0af432e18d6ff9bf364390ec1d33c6
Sabar <31942277+sabarop@users.noreply.github.com>
2026-08-27 09:49:05 +0700

    platina: change kernel target

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_CONFIG := platina_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/platina
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 
 # Media
 TARGET_USES_ION := true
